9f07ecdc97ccac493ee31e0c3690e939a01b134e
Yang HG <29534214+yanghg-basefx@users.noreply.github.com>
2019-03-19 09:32:22 +0800

    Update hybrid.py

--- graphene_sqlalchemy_ext/hybrid.py
+++ graphene_sqlalchemy_ext/hybrid.py
@@ -5,6 +5,31 @@ __all__ = ['declared_hybrid_property']
 
 
 class declared_hybrid_property(hybrid_property):
+    """
+    support other types in sqlalchemy models instead of only string
+    
+    class Model1:
+        name = String()
+    
+    class Model2:
+        name = String()
+    
+    class Model3:
+        m1 = relationship(Model1)
+        m2 = relationship(Model2)
+        
+        @declared_hybrid_property
+        def choose_m(self):
+            if self.id == 1:
+                return self.m1
+            elif self.id == 2:
+                return self.m2
+        
+        @choose_m.return_type
+        def choose_m(self):
+            return Model1, Model2
+    """
+    
     def __init__(self, fget, fset=None, fdel=None,
                  expr=None, custom_comparator=None, update_expr=None, return_type_func=None):
         super(declared_hybrid_property, self).__init__(fget=fget, fset=fset, fdel=fdel, expr=expr,
